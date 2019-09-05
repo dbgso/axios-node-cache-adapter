2faa2a6c397f8852753c59f000bbe2304e820f03
dbgso <s092018@gmail.com>
2019-09-06 01:02:18 +0900

    add config

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,22 @@ function str2Sha512(data: string) {
   return hash.digest('hex');
 }
 export interface CacheConfig {
-  enable: boolean,
-  dirPath: string | undefined
+  enable?: boolean,
+  dirPath?: string
 }
 
 export function createConfig(adapterConfig: CacheConfig) {
   const axiosCacheAdapter: AxiosAdapter = async (config: AxiosRequestConfig) => {
     const sha512 = str2Sha512(`${config.url} ${config.method}`);
 
-    const dir = `./.cache/`;
+    const enabled = adapterConfig.enable === undefined ? true : adapterConfig.enable;
+    const dir = adapterConfig.dirPath || `./.cache/`;
     mkdirSync(dir, {
       recursive: true
     })
     const cacheFilePath = `${dir}/${sha512}.json`;
 
-    if (existsSync(cacheFilePath) || !adapterConfig.enable) {
+    if (existsSync(cacheFilePath) || enabled) {
       const cachedResponse: AxiosResponse = JSON.parse(readFileSync(cacheFilePath).toString('UTF-8'));
       console.log("[cached]: ", config.url)
       return Promise.resolve(cachedResponse);
