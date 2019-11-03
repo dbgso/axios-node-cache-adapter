fe6fbf57810158eecc409f623c74c779067843c3
dbgso <s092018@gmail.com>
2019-11-03 11:19:03 +0900

    Add: test code

---
+++ __tests__/index.test.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosInstance } from "axios";
+import { createConfig } from "../src/index";
+import fs from "fs"
+import del from "del";
+
+describe("responseType null", () => {
+    const io = axios.create({
+        adapter: createConfig({
+            // enable: false
+        }),
+    })
+    beforeAll(async () => {
+        await del(['.cache'])
+    })
+
+    test('utf', async () => {
+        await request(io, 'http://localhost:8001/utf.html')
+    })
+    test('euc', async () => {
+        await request(io, 'http://localhost:8001/euc.html')
+    })
+    test('Shift-JIS', async () => {
+        await request(io, 'http://localhost:8001/shiftjis.html')
+    })
+})
+
+describe('responseType arraybuffer', () => {
+    test('arraybuffer', async () => {
+        const arrayBufferIo = axios.create({
+            adapter: createConfig({
+            }),
+            responseType: "arraybuffer"
+        })
+        await request(arrayBufferIo, 'http://localhost:8001/utf.html');
+    })
+
+    test('image', async () => {
+        const arrayBufferIo = axios.create({
+            adapter: createConfig({
+            }),
+            responseType: "arraybuffer"
+        })
+        await request(arrayBufferIo, 'http://localhost:8001/logo.png');
+    })
+
+})
+
+async function request(io: AxiosInstance, url: string) {
+    const noncached = await io.get<string>(url);
+    expect(noncached.status).toBe(200);
+    const cached = await io.get<string>(url);
+    expect(cached.status).toBe(200);
+    expect(noncached.data).toStrictEqual(cached.data);
+}
+
